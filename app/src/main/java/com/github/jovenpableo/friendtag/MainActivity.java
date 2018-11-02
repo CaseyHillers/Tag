a4d47133b2b6f71d09dcda2bf912b335d7d0674d
Casey Hillers <caseyhillers@gmail.com>
2018-11-01 20:28:59 -0700

    Firebase base + Map demo

---
+++ app/src/main/java/com/github/jovenpableo/friendtag/MainActivity.java
@@ -0,0 +1,10 @@
+package com.github.jovenpableo.friendtag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    private static int RC_SIGN_IN = 100;
+}
