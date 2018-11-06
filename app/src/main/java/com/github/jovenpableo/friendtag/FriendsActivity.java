1352d5ece49f044327d5bbab1b90f4379ff26b88
Joven Pableo <jpableo688@gmail.com>
2018-11-05 20:19:21 -0800

    commit beginning friends activity

---
+++ app/src/main/java/com/github/jovenpableo/friendtag/FriendsActivity.java
@@ -0,0 +1,15 @@
+package com.github.jovenpableo.friendtag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jovenpableo.friendtag.R;
+
+public class FriendsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends);
+    }
+}
