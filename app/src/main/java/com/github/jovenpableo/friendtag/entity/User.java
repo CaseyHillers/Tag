6065dbe115159bbcac92f899e83fe47ef71283fd
Casey Hillers <caseyhillers@gmail.com>
2018-11-08 19:20:11 -0800

    User helper methods

--- app/src/main/java/com/github/jovenpableo/friendtag/entity/User.java
+++ app/src/main/java/com/github/jovenpableo/friendtag/entity/User.java
@@ -6,13 +6,14 @@ import android.location.Location;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class User {
-    public String uid;
+    private String uid;
     private String displayName;
+
+    // NOTE: Lon and Lat are stored as doubles.
     private Location location;
     private Bitmap picture;
 
@@ -23,6 +24,15 @@ public class User {
         uid = firebaseUser.getUid();
         displayName = firebaseUser.getDisplayName();
     }
+
+    public String getUid() { return this.uid; }
+    public void setUid(String uid) { this.uid = uid; }
+    public String getDisplayName() { return this.displayName; }
+    public void setDisplayName(String displayName) { this.displayName = displayName; }
+    public Location getLocation() { return this.location; }
+    public void setLocation(Location location) { this.location = location; }
+    public Bitmap getPicture() { return this.picture; }
+    public void setPicture(Bitmap picture) { this.picture = picture;}
 
     public Map<String, Object> toMap() {
         HashMap<String, Object> map = new HashMap<>();
