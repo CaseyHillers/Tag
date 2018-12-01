5eeff1d445a8c62a0fcea61b49714b887fe80126
Casey Hillers <caseyhillers@gmail.com>
2018-12-01 15:50:47 -0800

    Profile custom built not using builder
    Couldn't add tag functionality into the builder

--- app/src/main/java/com/github/jovenpableo/friendtag/ProfileActivity.java
+++ app/src/main/java/com/github/jovenpableo/friendtag/ProfileActivity.java
@@ -1,41 +1,84 @@
 package com.github.jovenpableo.friendtag;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.location.Location;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.jovenpableo.friendtag.R;
 import com.github.jovenpableo.friendtag.entity.User;
-import com.github.jovenpableo.friendtag.firebase.Users;
-import com.vansuita.materialabout.builder.AboutBuilder;
-import com.vansuita.materialabout.views.AboutView;
+import com.github.jovenpableo.friendtag.firebase.UserManager;
 
 
 public class ProfileActivity extends AppCompatActivity {
+    private static String TAG = "ucsc-tag";
+
+    ImageView avatarView;
+    TextView nameView;
+    TextView bioView;
+
+    UserManager userManager;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Users users = new Users();
-        User user = users.getUser();
+        setContentView(R.layout.activity_profile);
 
-        Location location = users.getLocation(this);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        avatarView = findViewById(R.id.imageAvatar);
+        nameView = findViewById(R.id.textName1);
+        bioView = findViewById(R.id.textBio);
+
+        userManager = new UserManager();
+        user = userManager.getUser();
+
+        // NOTE: This is just here to update the location of a user
+        Location location = userManager.getLocation(this);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        setProfile(user);
+    }
+
+    private void setProfile(User user) {
+        User currentUser = userManager.getUser();
+        if (currentUser.equals(user)) {
+            Button actionButton = findViewById(R.id.btnAction);
+            FloatingActionButton floatingActionButton = findViewById((R.id.floatingActionButton));
+            actionButton.setVisibility(View.GONE);
+            floatingActionButton.setImageResource(R.drawable.ic_edit_white_24dp);
+        }
 
         String name = user.getDisplayName();
+        Bitmap avatar = user.getPicture();
+        String bio = user.getBio();
+        if (bio == null || bio.equals("")) {
+            bio = "Hello! My name is " + name + " and I am ready to play some tag!";
+            user.setBio(bio);
+        }
 
-        AboutView view = AboutBuilder.with(this)
-                .setPhoto(R.mipmap.profile_picture)
-                .setCover(R.mipmap.profile_cover)
-                .setName(name)
-                .setBrief("I'm like mobile technologies. Ideas maker, curious and nature lover.")
-                .setAppIcon(R.mipmap.ic_launcher)
-                .setWrapScrollView(true)
-                .setLinksAnimated(true)
-                .setShowAsCard(false)
-                .build();
+        Log.i(TAG, "Name: " + nameView.getText());
 
-        addContentView(view, new ViewGroup.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+        nameView.setText(name);
+        bioView.setText(bio);
+
+        Log.i(TAG, "New name: " + nameView.getText());
     }
 }
