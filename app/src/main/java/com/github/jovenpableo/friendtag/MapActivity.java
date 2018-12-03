4b74de37e69d3e20a342027f39d7c8df586a9487
Joven Pableo <jpableo688@gmail.com>
2018-12-03 12:49:38 -0800

    commit circle avatars on map

--- app/src/main/java/com/github/jovenpableo/friendtag/MapActivity.java
+++ app/src/main/java/com/github/jovenpableo/friendtag/MapActivity.java
@@ -2,37 +2,61 @@ package com.github.jovenpableo.friendtag;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
+import android.media.Image;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 import com.example.jovenpableo.friendtag.R;
 import com.github.jovenpableo.friendtag.entity.User;
 import com.github.jovenpableo.friendtag.firebase.Users;
+import com.github.jovenpableo.friendtag.utility.DownloadImage;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
 import java.util.concurrent.Callable;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     Context ctx;
-    private GoogleMap mMap;
+    private static GoogleMap mMap;
 
-    private ArrayList<User> users;
-    private Users userHelper;
+    ConnectivityManager connectivityManager;
+    NetworkInfo networkInfo;
+    DownloadImage di;
+
+    private static ArrayList<User> users;
+    private static Users userHelper;
+    public static Bitmap bit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        networkInfo = connectivityManager.getActiveNetworkInfo();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -93,12 +117,26 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     public void renderUsers() {
         users = userHelper.users;
         Log.i("ucsc-tag", "Adding users to the map (size: " + users.size() + ")");
+
         for (User user : users) {
-            Location location = user.getLocation();
-            Log.i("ucsc-tag", "Putting " + user.getDisplayName() + " at " + location.getLatitude() + ", " + location.getLongitude());
-            LatLng loc = new LatLng(user.getLocation().getLatitude(), user.getLocation().getLongitude());
-            mMap.addMarker(new MarkerOptions().position(loc).title(user.getDisplayName()));
+            di = new DownloadImage();
+            di.uuid = user.getUid();
+            di.execute(user.getPictureUrl());
         }
     }
 
+    public static void renderAfter(Bitmap bitmap, String currUID){
+        users = userHelper.users;
+
+        for (User user : users) {
+            if(user.getUid().equals(currUID)){
+                Location location = user.getLocation();
+
+                Log.i("ucsc-tag", "Putting " + user.getDisplayName() + " at " + location.getLatitude() + ", " + location.getLongitude());
+                LatLng loc = new LatLng(user.getLocation().getLatitude(), user.getLocation().getLongitude());
+                mMap.addMarker(new MarkerOptions().position(loc).title(user.getDisplayName()).icon(BitmapDescriptorFactory.fromBitmap(bitmap)));
+            }
+        }
+
+    }
 }
